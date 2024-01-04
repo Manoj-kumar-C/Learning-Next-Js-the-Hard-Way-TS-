82fd59c40936fa58fbe6e36b70eb0d5974e86b75
manoj-chandran <developermanoj2003@gmail.com>
2024-01-04 22:02:30 +0530

    alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AccordionDemo } from './shadcnui/AccorianDemo'
+import { AlertDemo } from './shadcnui/AlertDemo'
 
 function page() {
   return (
-    <div>
-      <AccordionDemo />
+    <div className='m-5 p-5'>
+      <AlertDemo />
     </div>
   )
 }
--- src/app/shadcnui/page.tsx
+++ src/app/shadcnui/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
+    <div className='center ml-2 m-5 p-4'>
         <h1>ShadCN UI </h1>
     </div>
   )
